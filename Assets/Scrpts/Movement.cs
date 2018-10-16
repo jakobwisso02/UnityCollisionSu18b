a1a814c2850c9a076574e00b9b9439d5ee086b38
Jakob Wissö <jakob.wisso@elev.ga.lbs.se>
2018-10-16 10:49:03 +0200

    Maze
    Next Up: Game Over skärm

---
+++ Assets/Scrpts/Movement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour
+{
+
+
+
+    public Rigidbody2D rbody;
+
+
+    void Start()
+    {
+
+        //rbody.AddForce(new Vector2(0f, 10f), ForceMode2D.Impulse); // Player får direkt x antal newton i y-axeln utan accelerering // void Start = Kraft direkt
+
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.R))
+        {
+
+            SceneManager.LoadScene("SampleScene");
+
+        }
+
+    }
+
+    // Allt med fysik att göra läggs här
+    void FixedUpdate() // Som update bara att den uppdaterar konstant 50 ggr
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            rbody.AddForce(new Vector2(10f, 0f));
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            rbody.AddForce(new Vector2(-10f, 0f));
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            rbody.AddForce(new Vector2(0f, 10f));
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            rbody.AddForce(new Vector2(0f, -10f));
+
+        }
+
+        // rbody.AddForce
+        // rbody.AddRelativeForce
+        // rbody.AddTorque
+        // rbody.MovePosition
+        // rbody.MoveRotation
+        // rbody.velocity
+        // AddForce är najs när man ska putta saker
+        // void FixedUpdate = Kraft efter tid
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        SceneManager.LoadScene("SampleScene");
+       
+    }
+
+
+
+
+}
