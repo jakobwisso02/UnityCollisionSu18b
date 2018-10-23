7db7d24fe325b01ebfea5878cf1b4aebabae36f6
Jakob Wissö <jakob.wisso@elev.ga.lbs.se>
2018-10-23 07:59:47 +0200

    Förra lektionen

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("EPIC", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("EPIC", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            anim.SetTrigger("trigga");
+        }
+
+    }
+
+    void MinCoolaTestFunktion()
+    {
+        print("MY MANGO IS TO BLOW UP, AND ACT LIKE I DONT KNOW NOBADY EH EH EH EH");
+        print("ERIK EKORRE STAL MIN MAYMAY"); 
+    }
+
+}
